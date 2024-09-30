6afbd6d80da06b2984eb4abc3d152ab59efb4389
= <mfittazim@gmail.com>
2024-10-01 01:25:11 +0600

    final

--- script.js
+++ script.js
@@ -1,37 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
 
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    
-    
-    if (window.scrollY > 0) {
-        navbar.classList.add('scrolled'); // Add class when scrolled
-       
-    } else {
-        navbar.classList.remove('scrolled'); // Remove class when at top
-        
-    }
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+  hamburger.classList.toggle('active');
 });
-
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-document.querySelector('.menu-bar').addEventListener('click', function(){
-    const mobilenav = document.getElementById('mobile-nav-show-off');
-    // console.log(mobilenav.classList.contains('hidden'));
-    if(mobilenav.classList.contains('hidden')) {
-        mobilenav.classList.remove('hidden');
-        // console.log('i am here');
-    }
-    else {
-        mobilenav.classList.add('hidden');
-        // console.log('i am not')
-    }
-})
